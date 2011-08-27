4ae8d27aab84ff9e0fc9174d29432e612e9a4887
Zac Zheng <zac@zenpow.com>
2011-08-27 21:03:14 +0100

    added instructions to Guardfile

--- Guardfile
+++ Guardfile
@@ -1,5 +1,9 @@
 # More info at https://github.com/guard/guard#readme
 
+# Instructions:
+# 1) gem install guard guard-shell
+# 2) run guard in root
+
 # run test with 'redgreen' gem(for colour) if available otherwise with plain ruby
 guard "shell" do
   watch(%r{^(.+)/.+\.rb$}) do |m|
--- matcher/matcher.rb
+++ matcher/matcher.rb
@@ -1,15 +1,28 @@
 class Matcher
   def match(expected, actual, clip_limit, delta) 
-    # Clip "too-large" values
-    actual = actual.map { |val| [val, clip_limit].min }
+    actual = clip(actual, clip_limit)
 
-    # Check for length differences
-    return false if actual.length != expected.length
+    return false if different?(actual, expected)
 
     # Check that each entry is within expected +/- delta
-    actual.each_index { |i|
-      return false if (expected[i] - actual[i]).abs > delta
-    }
+    return false if out_of_range?(expected, actual, delta)
+
     return true
+  end
+
+  def clip(array, limit)
+    array.map { |val| [val, limit].min }
+  end
+
+  def different?(actual, expected)
+    actual.length != expected.length
+  end
+
+  def out_of_range?(expected, actual, delta)
+    actual.each_index do |i|
+      return true if(expected[i] - actual[i]).abs > delta
+    end
+
+    false
   end
 end
